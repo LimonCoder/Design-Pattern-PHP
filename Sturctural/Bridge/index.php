c2758d2b1d1db4a498e2e9d0ef47056849ad94da
LIMON <limonfpi408@gmail.com>
2023-07-29 14:40:00 -0400

    bridge code modification

--- Sturctural/Bridge/index.php
+++ Sturctural/Bridge/index.php
@@ -1,48 +1,69 @@
 <?php
+
 # interface
-interface PaymentGateway{
+interface PaymentGateway
+{
     public function paymentProcess($amount);
 }
 
-class Bkash implements PaymentGateway{
+class Bkash implements PaymentGateway
+{
     public function paymentProcess($amount): string
     {
+        //TODO:: internal processing system
         return "$amount Tk pay in Bkash";
     }
 }
 
-class Nagad implements PaymentGateway{
+class Nagad implements PaymentGateway
+{
     public function paymentProcess($amount): string
     {
+        //TODO:: internal processing system
         return "$amount Tk pay in Nagad";
     }
 }
 
-abstract class Product{
+abstract class Product
+{
     public $gateway;
     public $name;
     public $price;
+
     public function __construct(PaymentGateway $gateway)
     {
         $this->gateway = $gateway;
     }
 
-    public function pay(){
+    public function pay()
+    {
         return $this->gateway->paymentProcess($this->price);
     }
 }
 
-class Electornics extends Product{
-    public $inch ;
+class Electornics extends Product
+{
+    public $inch;
 
-    public function __construct(PaymentGateway $gateway,string $inch)
+    public function __construct(PaymentGateway $gateway, string $inch)
     {
         parent::__construct($gateway);
         $this->inch = $inch;
     }
 }
 
-$object = new Electornics((new Nagad()),"34'");
+class Toy extends Product{
+
+}
+
+# Example 1 :
+$object = new Electornics((new Nagad()), "34'"); # 'Nagad' payment gateway in loosely coupled in Electornics Class
 $object->name = "ASUS TV";
 $object->price = 56454;
+echo $object->pay();
+
+# Example 2 :
+$object = new Toy((new Bkash())); # 'Bkash' payment gateway in loosely coupled in Toy Class
+$object->name = "Car";
+$object->price = 120;
 echo $object->pay();
